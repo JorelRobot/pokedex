77d5e720dd86c7e28cc4312585d79e92cce2436d
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-11-18 12:34:50 -0600

    ADD: Styles to the pokemon details page

--- src/Components/Layout/MainContent.js
+++ src/Components/Layout/MainContent.js
@@ -6,8 +6,8 @@ const MainContent = () => {
 
     return (
         <main>
-            <MainPage />
-            {/* <PokemonDetails /> */}
+            {/* <MainPage /> */}
+            <PokemonDetails />
         </main>
     );
 };
--- src/Components/Views/PokemonDetails.js
+++ src/Components/Views/PokemonDetails.js
@@ -3,15 +3,29 @@ import classes from './PokemonDetails.module.css';
 const PokemonDetails = () => {
     return (
         <>
-            <div className='details-header'>
-                <img src='' alt='Pokemon' />
-                <h1>Pokemon name</h1>
-                <ul className='pokemon-type'>
+            <div className={classes.container + ' ' + classes['details-header']}>
+                <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png' alt='Pokemon' />
+                <h1>Pikachu</h1>
+                <p>#25</p>
+                <ul className={classes['pokemon-type']}>
                     <li>Type</li>
                     <li>Type</li>
                     <li>Type</li>
                 </ul>
-                <p>Pokemon number</p>
+                <div className={classes['pokemon-section']}>
+                    <h3>Moves</h3>
+                    <ul>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                        <li>Move X</li>
+                    </ul>
+                </div>
             </div>
         </>
     );
